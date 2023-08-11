3c0a854a3870ec6ebfc3941d98f8ac8bbd883806
Irtaza Hyder <irtazahyder2002@gmail.com>
2023-08-11 15:48:20 +0500

    Roller implemented

---
+++ Assets/Prefabs/NewPlayer/BrushControllerv2.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushControllerv2 : MonoBehaviour
+{
+    //This script is used with co-ordination with the Unity's new input system and character controller
+    // Start is called before the first frame update
+    private Player player;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+    private void Awake()
+    {
+        player = new Player();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        player.Disable();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = player.Move.Movement.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is with an object tagged as "Enemy"
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy the enemy GameObject
+            Destroy(collision.gameObject);
+        }
+    }
+}
